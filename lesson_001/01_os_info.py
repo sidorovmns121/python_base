220db49cde3d6fcd476bd801e483ef5d97c8146b
sidorovmns121 <64742041+sidorovmns121@users.noreply.github.com>
2020-05-04 12:58:28 +0400

    Add files via upload

---
+++ lesson_001/01_os_info.py
@@ -0,0 +1,6 @@
+[InternetShortcut]
+URL=https://github.com/zaboevai/python_base/blob/develop/lesson_001/01_os_info.py
+IDList=
+HotKey=0
+IconFile=C:\Users\ololo\AppData\Local\Mozilla\Firefox\Profiles\0pcduked.default-release\shortcutCache\+Aj5+BDLKf0fK1bNWRpuvQ==.ico
+IconIndex=0
